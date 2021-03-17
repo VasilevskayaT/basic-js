a6be6d3d1f8e71b59238ea8e683c70c0ef71655d
VasilevskayaT <unost.work@gmail.com>
2021-03-18 01:09:36 +0300

    feat: implement dream-team and what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if(typeof date === 'object' && Object.keys(date).length !== 0) {
+        throw new Error('THROWN');
+    }
+
+    const month =  new Date(date).getMonth();
+    if(!date) return 'Unable to determine the time of year!';
+
+    switch (month) {
+        case 2:
+        case 3:
+        case 4:
+            return 'spring';
+            break;
+
+        case 6:
+        case 7:
+        case 5:
+        return 'summer';
+            break;
+
+        case 9:
+        case 10:
+        case 8:
+            return 'autumn';
+            break;
+        case 0:
+        case 1:
+        case 11:
+            return 'winter';
+            break;
+    default:
+        throw new Error('THROWN');
+    }
 };
