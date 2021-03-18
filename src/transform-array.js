44e3be9590c454b6b184114695583fa6bca26c31
VasilevskayaT <unost.work@gmail.com>
2021-03-18 23:04:54 +0300

    feat: inplement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw new Error('THROW');
+    const conditions = ['--double-next', '--discard-next', '--double-prev', '--discard-prev', 'Del']
+    let newArr = [...arr];
+    let newNewArr = [];
+
+    newArr = newArr.map((item, i, array) => {
+    return (array[i]==='--double-next' && i!==array.length-1) ? array[i]=array[i+1] :
+    (array[i+1]==='--discard-prev') ? array[i]='Del' :
+        (array[i]==='--discard-next') ? array[i+1]='Del' :
+            (array[i]==='--double-prev' && i!==0) ? array[i]=array[i-1] :
+                array[i];
+});
+
+    for (let i = 0; i < newArr.length; i++) {
+    let item = newArr[i];
+        if(!conditions.includes(item)) newNewArr.push(item);
+
+
+}
+    return newNewArr
+}
